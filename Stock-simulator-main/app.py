aceb7a9c948173c1c725d32f51a248397dca5ac4
veera venkat neeli <167682436+venkat4191@users.noreply.github.com>
2025-11-29 13:50:35 +0530

    Update app.py

--- Stock-simulator-main/app.py
+++ Stock-simulator-main/app.py
@@ -1707,7 +1707,7 @@ def search_stock():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5012))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
 
 
